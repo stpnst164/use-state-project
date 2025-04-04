d617e4b10721c05913d1fed69e99c19b492e47c5
stpnst164 <anastasiia.stp170@gmail.com>
2025-04-04 18:58:00 +0700

    added second hook count

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 
 function App() {
   //useState hook
-  const[count, setCount] = useState(0); //0 - начальное значение
-  let prevState = 0; //предыдущее значение
+  const[count1, setCount1] = useState(0); //hook1
+  const[count2, setCount2] = useState(0); //hook2
 
   return (
     <>
-      <h1>Hello</h1>
-      <p>My name is Anastasiia Kharkovets</p>
-      <button
-        onClick={() =>
-          setCount(prevState => prevState + 1) //с каждым кликом увеличивается счетчик
-        }
-      >Count is: {count}</button>
+      <div className="player-card">
+        <div className="player-card__title">Дима</div>
+        <div 
+        className="player-card__counter"
+        >{count1}</div>
+        <button
+        onClick={() => setCount1(() => count1 + 1)}>+</button>
+        <button
+        onClick={() => setCount1(() => count1 - 1)}>-</button>
+      </div>
+      <div className="player-card">
+        <div className="player-card__title">Настя</div>
+        <div className="player-card__counter">{count2}</div>
+        <button
+        onClick={() => setCount2(() => count2 + 1)}>+</button>
+        <button
+        onClick={() => setCount2(() => count2 - 1)}>-</button>
+      </div>
     </>
   )
 }
