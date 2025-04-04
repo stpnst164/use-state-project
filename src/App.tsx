80a00cef554d55890c00ffca60e9abd13456ffbc
stpnst164 <anastasiia.stp170@gmail.com>
2025-04-04 11:46:33 +0700

    modified click setCount

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { useState } from "react";
 function App() {
   //useState hook
   const[count, setCount] = useState(0); //0 - начальное значение
+  let prevState = 0; //предыдущее значение
 
   return (
     <>
       <h1>Hello</h1>
       <p>My name is Anastasiia Kharkovets</p>
       <button
-        onClick={() => 
-          setCount(5)
+        onClick={() =>
+          setCount(prevState => prevState + 1) //с каждым кликом увеличивается счетчик
         }
-        >Count is: {count + 1}</button>
+      >Count is: {count}</button>
     </>
   )
 }
